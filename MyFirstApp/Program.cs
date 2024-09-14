1d6e2192c2d33262d55544e519459b3665bee084
KKalamees <karmo.kalamees@gmail.com>
2024-09-14 12:46:39 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//This is a comment
+
+//rakendus küsib kasutajalt tema nime ja tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+string userName;
+
+userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}!");
